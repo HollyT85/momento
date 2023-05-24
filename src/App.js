810a9d2e71813a61a59d310fae0c800d0ebc640b
HollyT85 <h.e.turner@gmail.com>
2023-05-24 21:56:35 +0000

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import styles from './App.module.css'
 import NavBar from './components/NavBar';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Container className={styles.Main}>
+        <h1>Home</h1>
+        <h1>Sign In</h1>
+      </Container>
     </div>
   );
 }
