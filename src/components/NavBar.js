b2dc8fa20d815bfa1777cc26435559f836793fe2
HollyT85 <h.e.turner@gmail.com>
2023-05-30 21:41:48 +0000

    logout functionality created

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import { Container, Navbar, Nav} from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
-import { useCurrentUser } from '../contexts/CurrentUserContext'
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext'
+import Avatar from './Avatar'
+import axios from 'axios'
+
+
 
 const NavBar = () => {
 
-    const currentUser = useCurrentUser()
-    const loggedInIcons = (<>{currentUser?.username}</>)
+    const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
+
+    const handleSignOut = async () => {
+    try {
+        await axios.post("dj-rest-auth/logout/");
+        setCurrentUser(null);
+    } catch (err) {
+        console.log(err);
+    }
+    };
+
+    const loggedInIcons = (<>
+    <NavLink to="/feed">Posts</NavLink>
+    <NavLink to="/liked">Liked Posts</NavLink>
+    <NavLink to="/" onClick={handleSignOut}>Sign Out</NavLink>
+    <NavLink to={`/profiles/${currentUser?.profile_id}`}>Profile</NavLink>
+    <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+    {currentUser?.username}
+    </>)
     const loggedOutIcons = (<>
         <NavLink to="/signin">Sign In</NavLink>
         <NavLink to="/signup">Join</NavLink>
+    </>)
+    const addPostIcon = (<>
+        <NavLink to="/posts/create">New Post</NavLink>
     </>)
 
     return (
@@ -18,6 +43,7 @@ const NavBar = () => {
             <NavLink to="/">
                 <Navbar.Brand>Momento</Navbar.Brand>
             </NavLink>
+            {currentUser && addPostIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
