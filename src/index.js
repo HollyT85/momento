2c286ffd7b9cfdb73e85675066ae31e315017687
HollyT85 <h.e.turner@gmail.com>
2023-06-06 19:53:29 +0000

    still trying tofix error

--- src/index.js
+++ src/index.js
@@ -18,6 +18,24 @@ root.render(
     document.getElementById("root")  
 );
 
+
+//import { StrictMode } from 'react';
+//import { createRoot } from 'react-dom/client';
+
+//const rootElement = document.getElementById("root")
+//const root = createRoot(rootElement);
+
+//root.render(
+//  <StrictMode>
+//    <Router>
+//      <CurrentUserProvider>
+//        <App />
+//      </CurrentUserProvider>
+//    </Router>
+//  </StrictMode>,
+//  
+//);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
