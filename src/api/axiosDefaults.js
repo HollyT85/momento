f303a970174ed7a68b33fb06b1cc8d133a2f481a
HollyT85 <h.e.turner@gmail.com>
2023-06-15 17:11:59 +0000

    links

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://8000-hollyt85-api-s17ga60qnwc.ws-eu99.gitpod.io/";
+axios.defaults.baseURL = "https://drf-api-holly.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
